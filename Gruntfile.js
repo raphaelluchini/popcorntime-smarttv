99fd08da3cc2a8ff6a2eda5adc0eff4a8a69c3cc
raphaelluchini <laster_raphael@hotmail.com>
2014-11-21 19:05:03 -0200

    grunt express added.Use "grunt server" to test the app in your browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,12 +81,26 @@ module.exports = function(grunt) {
               verbose: false
           },
         },
+        express: {
+          options: {
+            // Override defaults here
+          },
+          dev: {
+            options: {
+              background:false,
+              script: 'server_local.js'
+            }
+          },
+      }
     });
+
+    grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-sync');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-samsungstv-packager');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.registerTask('server', ['express:dev']);
     grunt.registerTask('deploy', ['samsung']);
     grunt.registerTask('build', ['jshint', 'requirejs:dist', 'sync:assets', 'sync:src']);
     grunt.registerTask('default', ['build']);
